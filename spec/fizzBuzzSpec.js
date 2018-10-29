69019c3741d01ceca52a5ae109a0dce3bdf5ddcf
shannongamby <shannongamby@hotmail.com>
2018-10-29 17:13:39 +0000

    pairing complete

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,25 @@
+//Need test for 3 = fizz
+
+describe("fizzBuzz", function() {
+
+  it("returns 'fizz' when given 3", function() {
+    expect(fizzBuzz(3)).toEqual('fizz')
+  });
+
+  it("returns 'buzz' when given 3", function() {
+    expect(fizzBuzz(5)).toEqual('buzz')
+    });
+
+  it("returns 'fizz' when given 6", function() {
+    expect(fizzBuzz(6)).toEqual('fizz')
+  });
+
+  it("returns 'fizzbuzz' when given 15", function() {
+    expect(fizzBuzz(15)).toEqual('fizzbuzz')
+  });
+
+  it("returns 1 when given 1", function() {
+    expect(fizzBuzz(1)).toEqual(1)
+  });
+
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,9 @@
+function fizzBuzz(number) {
+  if (number % 15 === 0) return 'fizzbuzz';
+  if (number % 3 === 0) return 'fizz';
+  if (number % 5 === 0) return 'buzz';
+  return number;
+}
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz(i))
+}
